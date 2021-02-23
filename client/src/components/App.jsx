7744fb7ce4b7b4cecc419ea7f5cf5f935dd6fe52
Will <park.wongyun@gmail.com>
2021-02-23 15:40:28 -0800

    First Commit With Basic Front-End and Back-end Files

---
+++ client/src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  };
+
+  render() {
+
+    return (
+      <div id="components">
+		    <h1>Test</h1>
+      </div>
+    );
+  };
+};
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,6 @@
+const app = require('./app.js');
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
