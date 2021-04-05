5545f8d7308006e1aa950f240e248945a89aceb5
Will <park.wongyun@gmail.com>
2021-04-04 17:14:14 -0700

    Clear git cache

--- server/app.js
+++ server/app.js
@@ -1,20 +1,41 @@
-// ***** Dependencies *****
+// ========== Dependencies ==========
 const express = require('express');
 const path = require('path');
 const { addPadding } = require('../db/test_gen/dataHelperFunctions');
 const { pool } = require('../db/index.js');
 // require('dotenv').config();
 
-// ***** Express *****
+// ========== Express ==========
 const app = express();
 const DIST_DIR = path.join(__dirname, '..', 'client', 'dist');
 const indexHTML = path.join(__dirname, '..', 'client', 'dist', 'index.html');
-let idCounter = 1001;
 
 app.use(express.static(DIST_DIR));
 app.use('/:mode', express.static(indexHTML));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// ========== TEXT MODE ==========
+// === POSTS ID COUNTER ===
+let idCounter = '';
+app.get('/textmode/', (req, res) => {
+  const sql = `
+    SELECT id
+    FROM antoinette.posts 
+    ORDER BY id DESC 
+    LIMIT 1`;
+
+  pool.query(sql)
+    .then((result) => {
+      const { rows } = result;
+      console.log('result :', rows);
+      console.log('result type :', typeof rows);
+    })
+    .catch((error) => {
+      res.status(500).send();
+      console.log(error.stack);
+    });
+});
 
 app.get('/textmode/:sortBy', (req, res) => {
   const sql = 'SELECT * FROM antoinette.posts ORDER BY viewNum DESC LIMIT 6';
@@ -28,7 +49,6 @@ app.get('/textmode/:sortBy', (req, res) => {
       })
       .catch((error) => {
         res.status(500).send();
-        console.log(error.stack);
       });
   }
 });
