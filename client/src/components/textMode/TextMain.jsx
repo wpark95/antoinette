25b0b13645a290e969ecb39cb01bf077a127fa58
Will <park.wongyun@gmail.com>
2021-04-07 15:59:37 -0700

    Add input box clearing function after submit when creating text game

--- client/src/components/textMode/TextMain.jsx
+++ client/src/components/textMode/TextMain.jsx
@@ -1,33 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import axios from 'axios';
 import Header from '../Header';
 import TextMainPosts from './mainPosts/TextMainPosts';
 import './TextMain.css';
 
-class TextMain extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <h1>Text Main</h1>
-        <div className="text-main-controllers">
-          <Link to="/textmode/create">
-            <button id="create-game-btn" type="button">Create Game!</button>
-          </Link>
-          <TextMainPosts />
-        </div>
-
-      </div>
-
-    );
-  }
-}
-
+const TextMain = () => (
+  <div className="text-main">
+    <Header />
+    <h1>Text Main</h1>
+    <div className="text-main-controllers">
+      <TextMainPosts />
+      <Link to="/textmode/create">
+        <button id="create-game-btn" type="button">Create Game!</button>
+      </Link>
+    </div>
+  </div>
+);
 export default TextMain;
